b425f8a0aef05b847abbd52ec5b24e44c6321c77
Johnnie Q <johnnie.quatrevingt@opensky.com>
2017-07-31 17:13:54 -0700

    created FizzBuzz counting

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BintyLearning
+{
+    class Fizzbuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num < 51; num++)
+            {
+                if ((num % 5 == 0) && (num % 3 == 0))
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
